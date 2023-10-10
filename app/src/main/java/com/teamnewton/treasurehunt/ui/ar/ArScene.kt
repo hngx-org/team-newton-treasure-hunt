1104fbeeb28e506393473d155d6b6e7cda5fb6c6
toby1907 <olaleyetobi3@gmail.com>
2023-10-10 17:52:01 +0100

    added the SceneView depencencies

---
+++ app/src/main/java/com/teamnewton/treasurehunt/ui/ar/ArScene.kt
@@ -0,0 +1,35 @@
+package com.teamnewton.treasurehunt.ui.ar
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import io.github.sceneview.ar.ARScene
+import io.github.sceneview.ar.node.ArNode
+
+@Composable
+fun ARScreen() {
+    val nodes = remember { mutableStateListOf<ArNode>() }
+
+    Box(modifier = Modifier.fillMaxSize()) {
+        ARScene(
+            modifier = Modifier.fillMaxSize(),
+            nodes = nodes,
+            planeRenderer = true,
+            onCreate = { arSceneView ->
+                // Apply your configuration
+            },
+            onSessionCreate = { session ->
+                // Configure the ARCore session
+            },
+            onFrame = { arFrame ->
+                // Retrieve ARCore frame update
+            },
+            onTap = { hitResult ->
+                // User tapped in the AR view
+            }
+        )
+    }
+}
